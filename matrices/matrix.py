bd649c2cb299916984ae5136db162336297feada
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-22 16:03:54 +0000

    Add matrix multiplication

--- matrices/matrix.py
+++ matrices/matrix.py
@@ -56,6 +56,26 @@ class Matrix:
         return Matrix(*new_rows)
 
 
+    def __mul__(self, other):
+        if isinstance(other, Matrix):
+            if not can_multiply(self, other):
+                raise MatrixError("Cannot multiply %s and %s." % (str(self), str(other)))
+            new_rows = []
+            columns = other.columns()
+            for rindex, row in enumerate(self._rows):
+                new_row = []
+                for column in columns:
+                    new_row.append(sum([val * column[index] for index, val in enumerate(row)]))
+                new_rows.append(new_row)
+            return Matrix(*new_rows)
+        else:
+            return Matrix(*[[val * other for val in row] for row in self._rows])
+
+
+    def __rmul__(self, other):
+        return Matrix(*[[val * other for val in row] for row in self._rows])
+
+
     def rows(self):
         return self._rows
 
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -112,3 +112,51 @@ class MatrixAdditionTests(TestCase):
         matrix2 = Matrix((0, -4, 3), (9, -4, -3))
         with self.assertRaises(MatrixError):
             matrix1 - matrix2
+
+
+
+class MatrixMultiplicationTests(TestCase):
+
+    def test_can_multiply_function(self):
+        matrix1 = Mock(Matrix)
+        matrix2 = Mock(Matrix)
+        matrix1.size.return_value = (4, 3)
+        matrix2.size.return_value = (3, 2)
+        self.assertTrue(can_multiply(matrix1, matrix2))
+        matrix1.size.return_value = (3, 4)
+        matrix2.size.return_value = (2, 3)
+        self.assertFalse(can_multiply(matrix1, matrix2))
+
+
+    def test_can_multiply_matrices(self):
+        matrix1 = Matrix((1, 2, 3), (4, 5, 6))
+        matrix2 = Matrix((7, 8), (9, 10), (11, 12))
+        matrix3 = matrix1 * matrix2
+        self.assertEqual(matrix3.rows(), ((58, 64), (139, 154)))
+
+
+    @patch("matrices.matrix.can_multiply")
+    def test_cannot_multiply_if_function_says_no(self, mock_check):
+        mock_check.return_value = False
+        matrix1 = Matrix((1, 2, 3), (4, 5, 6))
+        matrix2 = Matrix((7, 8), (9, 10), (11, 12))
+        with self.assertRaises(MatrixError):
+            matrix1 * matrix2
+
+
+    def test_matrix_multiplication_is_not_commutative(self):
+        matrix1 = Matrix((1, 2), (4, 5))
+        matrix2 = Matrix((100, -2), (4.4, 50))
+        self.assertNotEqual(matrix1 * matrix2, matrix2 * matrix1)
+
+
+    def test_can_multiply_matrices_by_scalar(self):
+        matrix = Matrix((1, 2, 3), (4, 5, 6))
+        self.assertEqual(
+         matrix * 3,
+         Matrix((3, 6, 9), (12, 15, 18))
+        )
+        self.assertEqual(
+         3 * matrix,
+         Matrix((3, 6, 9), (12, 15, 18))
+        )
