2dda2acffa3cdf193df75b62b7e7d5f59ac989bc
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-22 15:54:59 +0000

    Add basic Matrix class

---
+++ matrices/matrix.py
@@ -0,0 +1,21 @@
+class Matrix:
+
+    def __init__(self, *rows):
+        if not rows:
+            raise TypeError("Matrix needs at least one row")
+        clean_rows = []
+        row_length = -1
+        for row in rows:
+            if not isinstance(row, list) and not isinstance(row, tuple):
+                raise TypeError("Matrix needs lists or tuples")
+            if row_length != -1 and len(row) != row_length:
+                raise ValueError("All Matrix rows must be of equal length")
+            row_length = len(row)
+            clean_rows.append(tuple(row))
+        if row_length == 0:
+            raise TypeError("Matrix rows cannot be empty")
+        self._rows = tuple(clean_rows)
+
+
+    def __repr__(self):
+        return "<Matrix (%i×%i)>" % (len(self._rows), len(self._rows[0]))
---
+++ tests/test_matrix.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+from matrices.matrix import Matrix
+
+class MatrixCreationTests(TestCase):
+
+    def test_can_make_matrix(self):
+        matrix = Matrix((1, 2, 3), (4, 5, 6))
+        self.assertEqual(matrix._rows, ((1, 2, 3), (4, 5, 6)))
+
+
+    def test_all_sequences_converted_to_tuple(self):
+        matrix = Matrix([1, 2, 3], (4, 5, 6))
+        self.assertEqual(matrix._rows, ((1, 2, 3), (4, 5, 6)))
+
+
+    def test_matrix_needs_sequences(self):
+        with self.assertRaises(TypeError):
+            Matrix([1, 2, 3], "456")
+
+
+    def test_matrix_cannot_be_empty(self):
+        with self.assertRaises(TypeError):
+            Matrix()
+        with self.assertRaises(TypeError):
+            Matrix([], [])
+
+
+    def test_sequences_must_be_equal_in_length(self):
+        with self.assertRaises(ValueError):
+            Matrix([1, 2, 3], [4, 5])
+
+
+    def test_matrix_repr(self):
+        matrix = Matrix((1, 2, 3), (4, 5, 6))
+        self.assertEqual(str(matrix), "<Matrix (2×3)>")
