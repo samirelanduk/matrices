4612d1507ffc47bdf96014a1b7f8bb3b86b85324
Sam Ireland <sam.ireland.uk@gmail.com>
2017-02-22 15:46:10 +0000

    First commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+ name="matrices",
+ version="0.1.0",
+ description="A pure-Python matrix processing library",
+ url="https://matrices.samireland.com,
+ author="Sam Ireland",
+ author_email="mail@samireland.com",
+ license="MIT",
+ classifiers=[
+  "Development Status :: 4 - Beta",
+  "Intended Audience :: Science/Research",
+  "License :: OSI Approved :: MIT License",
+  "Topic :: Scientific/Engineering :: Mathematics",
+  "Programming Language :: Python :: 3",
+  "Programming Language :: Python :: 3.0",
+  "Programming Language :: Python :: 3.1",
+  "Programming Language :: Python :: 3.2",
+  "Programming Language :: Python :: 3.3",
+  "Programming Language :: Python :: 3.4",
+  "Programming Language :: Python :: 3.5",
+  "Programming Language :: Python :: 3.6",
+ ],
+ keywords="matrix matrices",
+ packages=["matrices"]
+)
