2f908ef83b72670e51692b71b17b7ef0aa8a5b1b
Sam Ireland <sam.ireland.uk@gmail.com>
2017-08-22 15:03:42 +0100

    Start 0.2

--- matrices/__init__.py
+++ matrices/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "Sam Ireland"
 
 from .matrix import Matrix
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
  name="matrices",
- version="0.1.0",
+ version="0.2.0",
  description="A pure-Python matrix processing library",
  url="https://matrices.samireland.com",
  author="Sam Ireland",
